beeef38863cb705dcaf13c1cb7dcf42e3415cc16
amirvezvani <amir.vezvani.hossain.13.88@gmail.com>
2025-07-19 20:48:19 -1200

    init002

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import PostModel,CommetModel
 
 # Register your models here.
+m=[PostModel,CommetModel]
+admin.site.register(m)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class PostModel(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class CommetModel(models.Model):
+    post = models.ForeignKey(PostModel, on_delete=models.CASCADE)
+    body = models.TextField()
+    is_validate = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.body[:50]}'
